4674452c72b97af601def0b167624d9b0e4de501
sw-yx <shawnthe1@gmail.com>
2021-12-26 03:17:03 +0800

    comment out weirdness from custom folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,26 @@ set -eu -o pipefail
 
 echo "hi from entrypoint.sh"
 
-export BIND_ON_IP="${BIND_ON_IP:-$(hostname -i)}"
+# from https://github.com/arniebilloo/test/blob/8e52f7b1647c7b0f55b67f432358936f863ae1a4/multiple.Dockerfile
+# export BIND_ON_IP="${BIND_ON_IP:-$(hostname -i)}"
 
-if [[ "${BIND_ON_IP}" =~ ":" ]]; then
-    # ipv6
-    export TEMPORAL_CLI_ADDRESS="[${BIND_ON_IP}]:7233"
-else
-    # ipv4
-    export TEMPORAL_CLI_ADDRESS="${BIND_ON_IP}:7233"
-fi
+# if [[ "${BIND_ON_IP}" =~ ":" ]]; then
+#     # ipv6
+#     export TEMPORAL_CLI_ADDRESS="[${BIND_ON_IP}]:7233"
+# else
+#     # ipv4
+#     export TEMPORAL_CLI_ADDRESS="${BIND_ON_IP}:7233"
+# fi
 
-dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/config/docker.yaml
+# dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/config/docker.yaml
+
+# # Automatically setup Temporal Server (databases, Elasticsearch, default namespace) if "autosetup" is passed as an argument.
+# exec /etc/temporal/custom-auto-setup.sh
+
+# exec /etc/temporal/start-temporal.sh
+
 
 # Automatically setup Temporal Server (databases, Elasticsearch, default namespace) if "autosetup" is passed as an argument.
-exec /etc/temporal/custom-auto-setup.sh
+exec ./custom-auto-setup.sh
 
-exec /etc/temporal/start-temporal.sh
+exec ./start-temporal.sh
