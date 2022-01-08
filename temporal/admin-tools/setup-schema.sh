46e3c86b6c4fb3d740dd2a16853e2f309fbcaf59
Anurag Goel <anurag@render.com>
2022-01-07 17:20:34 -0800

    Supply password to admin-setup

--- temporal/admin-tools/setup-schema.sh
+++ temporal/admin-tools/setup-schema.sh
@@ -3,12 +3,12 @@
 TEMPORAL_HOME=${TEMPORAL_HOME:-/etc/temporal}
 SCHEMA_DIR=${TEMPORAL_HOME}/schema/postgresql/v96/temporal/versioned
 
-temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${DBNAME}" setup-schema -v 0.0
-temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${DBNAME}" update-schema -d "${SCHEMA_DIR}"
+temporal-sql-tool --plugin postgres --endpoint "${POSTGRES_SEEDS}" --user "${POSTGRES_USER}" --password "$POSTGRES_PWD" --port "${DB_PORT}" --db "${DBNAME}" setup-schema -v 0.0
+temporal-sql-tool --plugin postgres --endpoint "${POSTGRES_SEEDS}" --user "${POSTGRES_USER}" --password "$POSTGRES_PWD" --port "${DB_PORT}" --db "${DBNAME}" update-schema -d "${SCHEMA_DIR}"
 VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/postgresql/v96/visibility/versioned
 
-temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
-temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
+temporal-sql-tool --plugin postgres --endpoint "${POSTGRES_SEEDS}" --user "${POSTGRES_USER}" --password "$POSTGRES_PWD" --port "${DB_PORT}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
+temporal-sql-tool --plugin postgres --endpoint "${POSTGRES_SEEDS}" --user "${POSTGRES_USER}" --password "$POSTGRES_PWD" --port "${DB_PORT}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
 
 echo "Database setup complete. Running tail -f /dev/null to keep container running."
 tail -f /dev/null
